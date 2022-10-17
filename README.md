e99c7399f981b0cf8c512af33fb00f2c3e1b7ca7
Miguel Ñancucheo <mnancuch@gmail.com>
2022-10-17 16:26:50 -0400

    primer commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Ejercicio JS onclick
+
+  Ejercicio de oncliclk, con uso de HTML, CSS y archivo JS. 
+  Se utilizo contenedores con Flex.
+  
---
+++ script.js
@@ -0,0 +1,14 @@
+// script del ejercio onclick()
+
+function cambio_login (element) {
+    element.innerText = "Logo Off";
+}
+
+
+function remover_boton (element) {
+    element.remove();
+}
+
+function alerta() {
+    alert("This page says Ninja was liked.");
+}
